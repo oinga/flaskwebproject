f2bc166f97af6bcb2971ca58e3a525c4114be37f
Ortiv Inga <ortiv@programmer.net>
2019-02-20 22:22:12 -0500

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -7,5 +7,5 @@ from FlaskWebProject1 import app
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
