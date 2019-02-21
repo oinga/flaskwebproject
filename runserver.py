0d4933cceb50c7a048c1d7f97edaa84dbe6521f3
Ortiv Inga <ortiv@programmer.net>
2019-02-20 22:13:43 -0500

    change port settings to run with heroku

--- runserver.py
+++ runserver.py
@@ -6,9 +6,6 @@ from os import environ
 from FlaskWebProject1 import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
